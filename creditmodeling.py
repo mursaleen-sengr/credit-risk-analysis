99cb60b715d35b60223d192e4ffe8e77977abc3d
Mursaleen793 <malikmursaleenawan911@gmail.com>
2024-05-06 18:59:02 +0500

    implemented feature engineering on dataset

--- creditmodeling.py
+++ creditmodeling.py
@@ -29,4 +29,119 @@ df1.describe()
 df2.info()
 df2.describe()
 
+#-99999 value is data integrity issue 
+df1.shape
+df1=df1.loc[df1['Age_Oldest_TL']!=-99999]
+
+#removing null values from df2
+columns_to_be_removed = []
+
+for i in df2.columns:
+    if df2.loc[df2[i] == -99999].shape[0] > 10000:
+        columns_to_be_removed .append(i)
+#removed columns having too many null values 
+df2 = df2.drop(columns_to_be_removed, axis =1)
+
+#rows which has more null values 
+for i in df2.columns:
+    df2 = df2.loc[ df2[i] != -99999 ]
+    
+#check null values 
+df1.isna().sum()
+
+print("df2 values ")
+df2.isna().sum()
+
+# Checking common column names ti merge both data set
+for i in list(df1.columns):
+    if i in list(df2.columns):
+        print (i)
+
+# Merge the two dataframes, inner join so that no nulls are present
+df = pd. merge ( df1, df2, how ='inner', left_on = ['PROSPECTID'], right_on = ['PROSPECTID'] )
+
+df.info()
+df.isna().sum()
+df.shape
+
+
+#we will divide the features into categorical and numerical data seprate 
+
+# check how many columns are categorical
+for i in df.columns:
+    if df[i].dtype == 'object':
+        print(i)
+
+
+df["MARITALSTATUS"].value_counts()
+
+# Chi-square test for feature selection
+for i in ['MARITALSTATUS', 'EDUCATION', 'GENDER', 'last_prod_enq2', 'first_prod_enq2']:
+    chi2, pval, _, _ = chi2_contingency(pd.crosstab(df[i], df['Approved_Flag']))
+    print(i, '---', pval)
+
+#since all the features have pval<=0.05 we will accept all
+
+
+
+
+# VIF for numerical columns
+numeric_columns = []
+for i in df.columns:
+    if df[i].dtype != 'object' and i not in ['PROSPECTID','Approved_Flag']:
+        numeric_columns.append(i)
+
+
+
+
+# VIF sequentially check
+
+vif_data = df[numeric_columns]
+total_columns = vif_data.shape[1]
+columns_to_be_kept = []
+column_index = 0
+
+
+
+for i in range (0,total_columns):
+    
+    vif_value = variance_inflation_factor(vif_data, column_index)
+    print (column_index,'---',vif_value)
+    
+    
+    if vif_value <= 6:
+        columns_to_be_kept.append( numeric_columns[i] )
+        column_index = column_index+1
+    
+    else:
+        vif_data = vif_data.drop([ numeric_columns[i] ] , axis=1)
+        
+
+
+# check Anova for columns_to_be_kept 
+
+from scipy.stats import f_oneway
+
+columns_to_be_kept_numerical = []
+
+for i in columns_to_be_kept:
+    a = list(df[i])  
+    b = list(df['Approved_Flag'])  
+    
+    group_P1 = [value for value, group in zip(a, b) if group == 'P1']
+    group_P2 = [value for value, group in zip(a, b) if group == 'P2']
+    group_P3 = [value for value, group in zip(a, b) if group == 'P3']
+    group_P4 = [value for value, group in zip(a, b) if group == 'P4']
+
+
+    f_statistic, p_value = f_oneway(group_P1, group_P2, group_P3, group_P4)
+
+    if p_value <= 0.05:
+        columns_to_be_kept_numerical.append(i)
+
+
+
+
+
+
 
