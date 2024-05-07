c04e1decd2cf6feac77056b81e6ba964e2f60c4d
Mursaleen793 <malikmursaleenawan911@gmail.com>
2024-05-07 21:20:52 +0500

    Machine learning Model Development

--- creditmodeling.py
+++ creditmodeling.py
@@ -184,10 +184,114 @@ df.info()
 
 df_encoded = pd.get_dummies(df, columns=['MARITALSTATUS','GENDER', 'last_prod_enq2' ,'first_prod_enq2'])
 
+df_encoded.info()
+k = df_encoded.describe()
+k
+
+#machine learning model fitting 
+
+y = df_encoded['Approved_Flag']
+x = df_encoded. drop ( ['Approved_Flag'], axis = 1 )
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+rf_classifier = RandomForestClassifier(n_estimators = 200, random_state=42)
+
+rf_classifier.fit(x_train, y_train)
+
+y_pred = rf_classifier.predict(x_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print ()
+print(f'Accuracy: {accuracy}')
+print ()
+precision, recall, f1_score, _ = precision_recall_fscore_support(y_test, y_pred)
+
+
+for i, v in enumerate(['p1', 'p2', 'p3', 'p4']):
+    print(f"Class {v}:")
+    print(f"Precision: {precision[i]}")
+    print(f"Recall: {recall[i]}")
+    print(f"F1 Score: {f1_score[i]}")
+    print()
+
+
+# 2. xgboost
+
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+
+xgb_classifier = xgb.XGBClassifier(objective='multi:softmax',  num_class=4)
+
+
+
+y = df_encoded['Approved_Flag']
+x = df_encoded. drop ( ['Approved_Flag'], axis = 1 )
+
+
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(y)
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y_encoded, test_size=0.2, random_state=42)
 
 
 
 
+xgb_classifier.fit(x_train, y_train)
+y_pred = xgb_classifier.predict(x_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print ()
+print(f'Accuracy: {accuracy:.2f}')
+print ()
+
+precision, recall, f1_score, _ = precision_recall_fscore_support(y_test, y_pred)
+
+for i, v in enumerate(['p1', 'p2', 'p3', 'p4']):
+    print(f"Class {v}:")
+    print(f"Precision: {precision[i]}")
+    print(f"Recall: {recall[i]}")
+    print(f"F1 Score: {f1_score[i]}")
+    print()
+
+
+# 3. Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+
+
+y = df_encoded['Approved_Flag']
+x = df_encoded. drop ( ['Approved_Flag'], axis = 1 )
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+dt_model = DecisionTreeClassifier(max_depth=20, min_samples_split=10)
+dt_model.fit(x_train, y_train)
+y_pred = dt_model.predict(x_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print ()
+print(f"Accuracy: {accuracy:.2f}")
+print ()
+
+precision, recall, f1_score, _ = precision_recall_fscore_support(y_test, y_pred)
+
+for i, v in enumerate(['p1', 'p2', 'p3', 'p4']):
+    print(f"Class {v}:")
+    print(f"Precision: {precision[i]}")
+    print(f"Recall: {recall[i]}")
+    print(f"F1 Score: {f1_score[i]}")
+    print()
+
+#xgboost is better as compare to other models 
+#random forest = 0.76 
+#xboost = 0.78
+#decission tree =0.71
+# further fine tuning , more feature engineering , scaling , graphs etc 
+df_encoded
+df_encoded['Approved_Flag'].value_counts()
 
 
 
